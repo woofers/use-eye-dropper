2622ff684901d0df99e3f0a4c2f378df79295527
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-11-21 16:38:43 -0800

    Add build config

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+import React, { Props, Component, SVGProps } from 'react'
+
+export type ColorSelectionOptions = {
+  signal?: AbortSignal
+}
+
+export type ColorSelectionResult = {
+  sRGBHex: string
+}
+
+type EyeDropperHooks = {
+  close: () => void
+  open: (options?: ColorSelectionOptions) => Promise<ColorSelectionResult>
+  isSupported: () => void
+}
+
+function useEyeDropper(): EyeDropperHooks
+
+export default useEyeDropper
