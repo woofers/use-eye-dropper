8b15924543500cc68e3a1af257eab9a33d7701aa
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-11-27 17:04:58 -0800

    Add better error message for unsupported browsers

--- src/use-eye-dropper.js
+++ src/use-eye-dropper.js
@@ -21,12 +21,18 @@ const anySignal = signals => {
 
 const isSupported = () => typeof window !== 'undefined' && 'EyeDropper' in window
 
-const noop = () => {}
+const resolveError = () => {
+  let error = 'Unsupported browser'
+  if (__isDev__) {
+    error = 'Unsupported browser: no EyeDropper in Window. Check https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper_API'
+  }
+  return Promise.reject(error)
+}
 
 const createInstance = options => isSupported() ? new EyeDropper(options) : {}
 
 const bindFunc = (key, instance) => {
-  if (!isSupported()) return noop
+  if (!isSupported()) return resolveError
   return EyeDropper.prototype[key].bind(instance)
 }
 
