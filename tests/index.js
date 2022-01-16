d8734a4e5843597139204a6582dce00660805175
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-01-15 16:07:40 -0800

    Re-factor tests to re-use component

--- tests/index.js
+++ tests/index.js
@@ -22,80 +22,68 @@ describe('EyeDropper Mock', () => {
 
 describe('useEyeDropper', () => {
   describe('open()', () => {
-    it('open() resolves color', async () => {
-      const Button = () => {
-        const [color, setColor] = useState('None')
-        const { open } = useEyeDropper()
-        return <button onClick={() => open().then(result => setColor(result.sRGBHex))}>{color}</button>
+    const Button = ({ onPick }) => {
+      const [color, setColor] = useState('None')
+      const { open } = useEyeDropper()
+      const onClick = () => {
+        const openPicker = async () => {
+          const controller = new AbortController()
+          const { signal } = controller
+          let result
+          try {
+            result = await onPick(open, controller)
+          }
+          catch (e) {
+            setColor(e.message)
+            return
+          }
+          setColor(result.sRGBHex)
+        }
+        openPicker()
       }
-      render(<Button />)
+      return <button onClick={onClick}>{color}</button>
+    }
+    it('open() resolves color', async () => {
+      const onPick = open => open()
+      render(<Button onPick={onPick} />)
       const button = screen.getByText('None')
       expect(button).toBeInTheDocument()
       fireEvent.click(button)
       await waitFor(() => expect(screen.getByText('rgba(255, 255, 255, 0)')).toBeInTheDocument())
     })
     it('open() does not resolve when called with an aborted signal', async () => {
-      const Button = () => {
-        const [color, setColor] = useState('None')
-        const { open } = useEyeDropper()
-        const onClick = () => {
-          const openPicker = async () => {
-            const controller = new AbortController()
-            controller.abort()
-            const { signal } = controller
-            let result
-            try {
-              result = await open({ signal })
-            }
-            catch (e) {
-              setColor(e.message)
-              return
-            }
-            setColor(result.sRGBHex)
-          }
-          openPicker()
-        }
-        return <button onClick={onClick}>{color}</button>
+      const onPick = (open, controller) => {
+        controller.abort()
+        return open({ signal: controller.signal })
       }
-      render(<Button />)
+      render(<Button onPick={onPick} />)
       fireEvent.click(screen.getByText('None'))
       await waitFor(() => expect(screen.getByText("Failed to execute 'open' on 'EyeDropper': Color selection aborted.")).toBeInTheDocument())
     })
     it('open() does not resolve when called with an aborted signal while open', async () => {
-      const Button = () => {
-        const [color, setColor] = useState('None')
-        const { open } = useEyeDropper()
-        const onClick = () => {
-          const openPicker = async () => {
-            const controller = new AbortController()
-            const { signal } = controller
-            open({ signal }).then(result => setColor(result.sRGBHex)).catch(e => setColor(e.message))
-            controller.abort()
-          }
-          openPicker()
-        }
-        return <button onClick={onClick}>{color}</button>
-      }
-      render(<Button />)
+      const onPick = (open, controller) =>
+        new Promise((resolve, reject) => {
+          open({ signal: controller.signal })
+            .then(result => resolve(result))
+            .catch(err => reject(err))
+          controller.abort()
+        })
+      render(<Button onPick={onPick} />)
       fireEvent.click(screen.getByText('None'))
       await waitFor(() => expect(screen.getByText("Color selection aborted.")).toBeInTheDocument())
     })
   })
   describe('isSupported()', () => {
+    const Hint = () => {
+      const { isSupported } = useEyeDropper()
+      return isSupported() ? 'EyeDropper API is supported' : 'EyeDropper API unavailable'
+    }
     it('isSupported() is truthy when supported', async () => {
-      const Hint = () => {
-        const { isSupported } = useEyeDropper()
-        return isSupported() ? 'EyeDropper API is supported' : 'EyeDropper API unavailable'
-      }
       render(<Hint />)
       expect(screen.getByText('EyeDropper API is supported'))
     })
     it('isSupported() is falsy when unsupported', async () => {
       delete window.EyeDropper
-      const Hint = () => {
-        const { isSupported } = useEyeDropper()
-        return isSupported() ? 'EyeDropper API is supported' : 'EyeDropper API unavailable'
-      }
       render(<Hint />)
       expect(screen.getByText('EyeDropper API unavailable'))
     })
