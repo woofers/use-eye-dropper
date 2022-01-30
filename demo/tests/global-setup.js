310c11c04149731e29fb3b3816a4608b7d7698a3
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-01-29 17:11:25 -0800

    Skip build

--- demo/tests/global-setup.js
+++ demo/tests/global-setup.js
@@ -4,7 +4,7 @@ const cli = require('next/dist/cli/next-build')
 const globalSetup = async () => {
   const dirname = path.resolve(path.dirname(''));
   process.env.PLAYWRIGHT = '1'
-  if (process.env.SKIP_BUILD === '1') {
+  if (process.env.SKIP_BUILD === 'true') {
     console.log('skipping build as SKIP_BUILD is set')
   } else {
     await cli.nextBuild([dirname])
