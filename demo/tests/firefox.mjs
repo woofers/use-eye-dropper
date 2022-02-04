0e2a7057d4eadbc4b474a30844e5832b32bc186a
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-02-04 00:00:25 -0800

    Add test to reject non supported browsers

--- demo/tests/firefox.mjs
+++ demo/tests/firefox.mjs
@@ -1,10 +1,15 @@
 import { expect } from '@playwright/test'
 import test from './next-fixture.mjs'
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
 const goto = async (page, port) =>
   page.goto(`http://localhost:${port}/use-eye-dropper/playground`)
+
+test('open() is rejected when EyeDropper API is not supported', async ({ page, port }) => {
+  await goto(page, port)
+  await page.locator('"Open"').click()
+  const msg = await page.innerText('div[aria-label="Status"]')
+  expect(msg).toBe('Unsupported browser.')
+})
 
 test('close() works when EyeDropper API is not supported', async ({ page, port }) => {
   await goto(page, port)
