8c43176da408193310ed2ca388f5079e32ed1f55
Jaxson Van Doorn <woofers1tt@gmail.com>
2023-07-30 17:39:26 +0000

    Fix coverage in GitHub Actions

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -44,8 +44,8 @@ module.exports = {
       '/features/': { page: '/[[...section]]' },
       '/usage/': { page: '/[[...section]]' },
       '/methods/': { page: '/[[...section]]' },
-      '/playground/': { page: 'playground' },
-      '/usage-with-typescript/': { page: 'usage-with-typescript' }
+      '/usage-with-typescript/': { page: '/[[...section]]' },
+      '/playground/': { page: 'playground' }
     }
   }
 }
