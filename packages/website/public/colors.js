35df8d4e1fe960dc6c441d27989f7319f72f40a6
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-08-04 23:21:52 +0000

    Add fallback color

--- packages/website/public/colors.js
+++ packages/website/public/colors.js
@@ -1,4 +1,3 @@
-
 const scale = (color, steps = 31) =>
   chroma.scale(['#fff', color, '#000']).mode('lch').colors(steps)
 
@@ -46,55 +45,72 @@ const toHex = color => chroma(color).hex()
 
 const alpha = (color, a) => chroma(color).alpha(a)
 
-const getColorsFromBase = (color) => {
-    const colors = scale(color)
-    const [backgroundColor, swap] = getBackground(colors)
-    const accent = getAccent(colors)
-    const lightText = swap ? color : accent
-    const text = !swap ? color : accent
-    const colorText = toHex(color)
-    const apple =
-        contrast('#FFF', backgroundColor) > 1.6 ? 'black-translucent' : 'default'
-    return {
-        color,
-        outline: accent,
-        background: backgroundColor,
-        background88: alpha(backgroundColor, 0.88),
-        background0: alpha(backgroundColor, 0),
-        lightText,
-        text,
-        colorText,
-        apple
-    }
+const getColorsFromBase = color => {
+  const colors = scale(color)
+  const [backgroundColor, swap] = getBackground(colors)
+  const accent = getAccent(colors)
+  const lightText = swap ? color : accent
+  const text = !swap ? color : accent
+  const colorText = toHex(color)
+  const apple =
+    contrast('#FFF', backgroundColor) > 1.6 ? 'black-translucent' : 'default'
+  return {
+    color,
+    outline: accent,
+    background: backgroundColor,
+    background88: alpha(backgroundColor, 0.88),
+    background0: alpha(backgroundColor, 0),
+    lightText,
+    text,
+    colorText,
+    apple
+  }
 }
 
-const setPropFromColor = (color) => {
-    const result = getColorsFromBase(color)
-    const element = document.body.children?.[0]
-    if (element) {
-      Object.keys(result).forEach(key => {
-        element.style.setProperty(`--dropper-${key}`, result[key])
-      })
-      document.body.style.backgroundColor = result.color
-      const button = document.getElementById("ued-color")
-      if (button) {
-        button.setAttribute("data-text", color)
-      }
+const setPropFromColor = color => {
+  const result = getColorsFromBase(color)
+  const element = document.body.children?.[0]
+  if (element) {
+    Object.keys(result).forEach(key => {
+      element.style.setProperty(`--dropper-${key}`, result[key])
+    })
+    document.body.style.backgroundColor = result.color
+    const button = document.getElementById('ued-color')
+    if (button) {
+      button.setAttribute('data-text', color)
     }
+  }
 }
 
 const ued = {
-    alpha,
-    toHex,
-    contrast,
-    scale,
-    getAccent,
-    getBackground,
-    setPropFromColor
+  alpha,
+  toHex,
+  contrast,
+  scale,
+  getAccent,
+  getBackground,
+  setPropFromColor
 }
 window.ued = ued
 
-document.addEventListener("DOMContentLoaded", () => {
-    !function(){try{var e,v,o=document,c=o.cookie,k=`; ${c}`.split("; color=");if(2===k.length&&(e=k.pop().split(";").shift())){v=JSON.parse(e);if(typeof v==="string"&&v.length===7&&!isNaN(Number("0x"+v.substring(1)))){window.UED_COLOR=v;ued.setPropFromColor(v);}}}catch(_){}}()
-});
-
+document.addEventListener('DOMContentLoaded', () => {
+  !(function () {
+    try {
+      var e, s, f, v, o = document, c = o.cookie, qk = `; ${c}`.split('; color=')
+      if (2 === k.length && (e = k.pop().split(';').shift())) {
+        v = JSON.parse(e)
+        if (
+          typeof v === 'string' &&
+          v.length === 7 &&
+          !isNaN(Number('0x' + v.substring(1)))
+        ) {
+          s = v
+          f = true
+        }
+      }
+      if (!f) s = '#0074e0'
+      window.UED_COLOR = s
+      ued.setPropFromColor(s)
+    } catch (_) {}
+  })()
+})
