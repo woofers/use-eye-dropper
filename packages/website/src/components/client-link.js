948fe5f17f6033919cc41120ce1ae327c72e5256
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-08-04 23:56:05 +0000

    Re-set button text

--- packages/website/src/components/client-link.js
+++ packages/website/src/components/client-link.js
@@ -1,7 +1,16 @@
 import Link from 'next/link'
 
+const onClick = () => {
+  if (typeof window !== 'undefined') {
+    const button = document.getElementById('ued-color')
+    if (button) {
+      button.setAttribute('data-text', window.UED_COLOR)
+    }
+  }
+}
+
 export const ClientLink = props => {
-  return <Link {...props} />
+  return <Link {...props} onClick={onClick} />
 }
 
 export default ClientLink
