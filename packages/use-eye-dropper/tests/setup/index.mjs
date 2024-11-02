81623606faa46f5c7dc49a168f9b572cd829b970
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-11-02 00:04:47 +0000

    Remove text encoder from test setup

--- packages/use-eye-dropper/tests/setup/index.mjs
+++ packages/use-eye-dropper/tests/setup/index.mjs
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/vitest'
-import { TextEncoder, TextDecoder, format } from 'util'
+import { format } from 'util'
 
 /* Replaces built-in functions */
 const replace = (obj, key, func) => {
@@ -18,8 +18,4 @@ replace(console, 'error', ([message, ...args], error) => {
   const formatted = format(message, ...args)
   throw message instanceof Error ? formatted : new Error(formatted)
 })
-
-/* Set up text encoder/decode for jsdom */
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
 
