6a4302a185750599a339c27561804ebd1ab19601
woofers <woofers1tt@gmail.com>
2024-12-30 04:19:24 +0000

    Updates

--- _next/static/3Aol-3oKuYGFLvUpBuD0Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1df04ee4953cabc9.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
