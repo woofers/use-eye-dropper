5da084395938361d87544f03746cfc86a06df547
woofers <woofers1tt@gmail.com>
2026-06-29 02:13:45 +0000

    Updates

---
+++ _next/static/qXnVcsY4XaxAus7VM6Wr5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/use-eye-dropper//_next/:path+",
+        "destination": "/use-eye-dropper/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
